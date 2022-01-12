96645d906860a9025833e35a158df49a73123c50
cuongnhph17594 <cuongnhph17594@fpt.edu.vn>
2022-01-12 19:23:57 +0700

    update-lab2

--- src/data.js
+++ src/data.js
@@ -1,21 +1,22 @@
 const data = [
     {
-        id: 1, title: "Bai viet 1", img: "https://picsum.photos/300/300", desc: "Nội dung bài viết 1",
+        id: "1",
+        title: "Vinh danh 295 sinh viên FPT Polytechnic học kì Spring 2018",
+        img: "https://picsum.photos/300/200",
+        desc: "Tối ngày 16/06/2018, Cao đẳng FPT Polytechnic Hà Nội đã tổ chức Lễ tôn vinh “Ong vàng Poly”,tuyên dương 295 cá nhân và tập thể có thành tích xuất sắc trong học tập cũng như hoạt",
     },
     {
-        id: 2, title: "Bai viet 2", img: "https://picsum.photos/300/300", desc: "Nội dung bài viết 2",
-    },
+        id: "2",
+        title: "Sinh viên khối ngành kinh tế học cách chạm vào cảm xúc",
+        img: "https://picsum.photos/300/200",
+        desc: "Với sự tham gia của chuyên gia trong lĩnh vực kinh doanh, chương trình thu hút đông đảo sinh viên Cao đẳng FPT Polytechnic tha dự. Đặc biệt các bạn sinh viên bị lôi cuốn bởi"
+     },
     {
-        id: 3, title: "Bai viet 3", img: "https://picsum.photos/300/300", desc: "Nội dung bài viết 3",
-    },
-    {
-        id: 4, title: "Bai viet 4", img: "https://picsum.photos/300/300", desc: "Nội dung bài viết 4",
-    },
-    {
-        id: 5, title: "Bai viet 5", img: "https://picsum.photos/300/300", desc: "Nội dung bài viết 5",
-    },
-    {
-        id: 6, title: "Bai viet 6", img: "https://picsum.photos/300/300", desc: "Nội dung bài viết 6",
-    },
-];
-export default data;
+        id: "3",
+        title: "Tiêu đề bài viết 3Sinh viên FPoly trải nghiệm thực tế tại Mường Thanh Luxury",
+        img: "https://picsum.photos/300/200",
+        desc: "Với triết lí đào tạo Thực học-Thực nghiệp, Cao đẳng FPT Polytechnic Đà Nẵng thường xuyên tổ chức cho sinh viên tham quan các đơn vị doanh nghiệp",
+      },
+   
+  ];
+  export default data;
--- src/main.js
+++ src/main.js
@@ -1,28 +1,25 @@
-// document.querySelector('#app').innerHTML = `
-//   <h1>Hello Vite!</h1>
-//   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-// `
-import Navigo from "navigo";
-import Footer from "./components/footer";
-import Header from "./components/header";
-import AboutPage from "./pages/about";
-import HomePage from "./pages/home";
+import Navigo from 'navigo';
+import Header from './components/header.js';
 
-const router = new Navigo("/", { linksSelector: "a" });
+import AboutPage from './pages/about.js';
+import HomePage from './pages/home.js';
 
-const render = (content) => {
-    document.getElementById("header").innerHTML = Header.print();
-    document.getElementById("content").innerHTML = content.print();
-    document.getElementById("footer").innerHTML = Footer.print();
+const router = new Navigo("/", {linksSelector: "a"});
+const render =  (content) => {
+   // console.log(content); //{print()}
+   document.getElementById("header").innerHTML = Header.print();
+    document.getElementById("app").innerHTML = content;
 };
 
 router.on({
     "/": () => {
-        render(HomePage);
+        render(HomePage.print());
     },
     "/about": () => {
-        render(AboutPage);
+        render( AboutPage.print());
+
     },
+    
 });
 
 router.resolve();
